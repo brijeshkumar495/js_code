302393165e31c98c5d02a643ff20c47fb92c568b
brijeshkumar495 <68318738+brijeshkumar495@users.noreply.github.com>
2023-08-11 13:03:03 +0000

    var

---
+++ 01_basics/var.js
@@ -0,0 +1,14 @@
+console.log("brijesh")
+ const accountid = 155216
+ let accountEmail ="jhgtg@jhhuf.com"
+ var accountpassword ="26526656"
+ accountcity= "jhkkgji"
+
+ accountid =23569566
+ accountEmail ="kjuink@kjg.com"
+ accountpassword ="65659569958"
+ accountcity ="juyujkljiy"
+
+
+ 
+console.log(typeof null);
