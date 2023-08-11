4ad18f63580ffe5ca785c9d303c6cce3e125e7f2
brijeshkumar495 <68318738+brijeshkumar495@users.noreply.github.com>
2023-08-11 13:02:44 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,13 @@
+"use strict"//treat all js code as newer version
+
+
+//number=>2 to power 53
+// BigInt
+// String
+// Boolean
+// null
+// undefined=>
+// symbol
+ let age =25
+
+console.log(typeof Object);
